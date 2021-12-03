a4306e5d09511e3b4b50051358198193c2be27a8
Soebb <87156166+Soebb@users.noreply.github.com>
2021-12-04 01:27:18 +0330

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ Send me a video to get started.
 
 START_BTN = InlineKeyboardMarkup(
         [[
-        InlineKeyboardButton("Source Code", url="https://github.com/samadii/VidSubExtract-Bot"),
+        InlineKeyboardButton("Source Code", url="https://github.com/Soebb/vidsub"),
         ]]
     )
 
@@ -44,7 +44,7 @@ async def start(bot, update):
 
 @Bot.on_message(filters.private & filters.video)
 async def main(bot, m):
-    ms = await m.reply("`Downloading and Extracting...`", parse_mode='md')
+    await m.reply("`Downloading and Extracting...`", parse_mode='md')
     await m.download("temp/vid.mp4")
     #if __name__ == '__main__':  # This check is mandatory for Windows.
     save_subtitles_to_file('temp/vid.mp4', 'temp/subtitle.srt', lang='fa', time_start='0:00', time_end='', conf_threshold=75, sim_threshold=80, use_fullframe=False, det_model_dir=None, rec_model_dir=None, use_gpu=False, brightness_threshold=None, similar_image_threshold=100, similar_pixel_threshold=25, frames_to_skip=1, crop_x=None, crop_y=None, crop_width=None, crop_height=None)
