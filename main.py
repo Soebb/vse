13fb1602995e0c3aded1b96d381bfc2f960241ea
Soebb <87156166+Soebb@users.noreply.github.com>
2022-01-01 18:09:06 +0330

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,30 @@ import os
 BOT_TOKEN = " "
 
 
+def start(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /start is issued."""
+    keyboard = []
+    keyboard.append(refresh_button)
+    try:
+        for file in glob.glob(vdir):
+            if file.endswith(('.ts', '.mp4', '.mkv')):
+                keyboard.append(
+                    [
+                        InlineKeyboardButton(
+                            text=file.rsplit('/', 1)[1].replace(main, ''),
+                            callback_data=file.rsplit('/', 1)[1].replace(main, '')
+                        )
+                    ]
+                )
+    except Exception as e:
+        print(e)
+        return
+    keyboard.append(refresh_button)
+    #await bot.send_message(chat_id=id, text="Which one?", reply_markup=InlineKeyboardMarkup(keyboard))
+    update.effective_message.reply_text(text="Which one?", reply_markup=InlineKeyboardMarkup(keyboard))
+
+
+
 
     output = input + '.srt'
     #if __name__ == '__main__':  # This check is mandatory for Windows.
